972475c3bdcdefe2f066bb55aa82ccc5613e3105
Robert Payne <robert.l.payne@lowes.com>
2015-01-13 14:00:10 -0500

    Initial Commit

---
+++ GGJ2015/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  GGJ2015
+//
+
+//  Copyright (c) 2015 Payne, Robert - Robert L. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
---
+++ GGJ2015/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GGJ2015
+//
+//  Created by Payne, Robert - Robert L on 1/13/15.
+//  Copyright (c) 2015 Payne, Robert - Robert L. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
